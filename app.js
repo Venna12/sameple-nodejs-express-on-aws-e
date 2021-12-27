a2f69413aaebc5b7c00c9a0fba3d6ee9ea2e72c2
Venna12 <52368332+Venna12@users.noreply.github.com>
2021-12-27 13:02:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.1</p>');
+  res.send('<h1>Welcome TO PERICIUS</h1>  <p>Version 1.1</p>');
 })
 
 app.get('/products', (req, res) => {
